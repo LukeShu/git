bac01b357073485701e9f77c2408f7937a04d351
Luke Shumaker <lukeshu@datawire.io>
2019-03-31 01:59:57 -0400

    subtree: don't let debug and progress output clash
    Currently, debug output (triggered by passing '-d') and progress output
    stomp on eachother.  The debug output is just streamed as lines to
    stderr, and the progress output is sent to stderr as '%s\r'.  It is
    difficult to distinguish between the debug output and a progress line.
    When writing to a terminal the debug lines hide progress lines.
    
    So, when '-d' has been passed, spit out progress as 'progress: %s\n',
    instead of as '%s\r', so that it can be detected, and so that the debug
    lines don't overwrite the progress when written to a terminal.
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- contrib/subtree/git-subtree.sh
+++ contrib/subtree/git-subtree.sh
@@ -53,7 +53,12 @@ debug () {
 progress () {
 	if test -z "$GIT_QUIET"
 	then
-		printf "%s\r" "$*" >&2
+		if test -n "$arg_debug"
+		then
+			printf "progress: %s\n" "$*" >&2
+		else
+			printf "%s\r" "$*" >&2
+		fi
 	fi
 }
 
